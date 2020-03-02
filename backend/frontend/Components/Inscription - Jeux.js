96154f33b91a583abda7957ceb45c1025d697d90
Jerome Perret <jeromeperret@yahoo.fr>
2020-03-02 15:28:33 +0100

    maj

--- backend/frontend/Components/Inscription - Jeux.js
+++ backend/frontend/Components/Inscription - Jeux.js
@@ -1,3 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Progress } from 'reactstrap';
+
+
+
+function Inscription() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+}
+
 // card
 
 // Plateforme 
@@ -6,14 +28,25 @@
 // Service
 // Champ de saisie
 
-// Jeux - image - API 
+// Jeux - image - API (route)
 // Champ de saisie
 
 // Tag
 // Champ de saisie
 
 // Progression bar
+const Example = (props) => {
+    return (
+      <div>
+        <Progress value="25">25%</Progress>
+      </div>
+    );
+  };
+  
 
-// Bouton Save
+// Bouton Save (route)
+<Button color="primary" size="lg">Save</Button>{' '}
 
 
+
+export default Inscription;
--- backend/frontend/Components/Match Page.js
+++ backend/frontend/Components/Match Page.js
@@ -1,3 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Match() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+}
+
 // Titre Your Player Two
 
 // Ajouter tout
@@ -42,3 +61,4 @@
       //Game's list backend
 
 
+      export default Match;

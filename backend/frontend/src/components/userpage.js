16131e4a67c98a7269f550f672acb8bc21b5e57f
Ragisacam <pbizet@gmail.com>
2020-03-02 12:22:52 +0100

    homepage et userpage

---
+++ backend/frontend/src/components/userpage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import '../App.css';
+import { 
+  Button,
+  Col,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText
+ } from 'reactstrap';
+
+
+/* question pour Quentin : 4 DIV à la suite ou séparation sur 2 colonnes de la page ? */
+
+{/* <CardBody>
+Image de l'avatar (chargé par le backend)
+Nom de l'utilisateur (chargé par le backend)
+Team (affiché par le backend uniquement si l'utilisateur est dans une ou plusieurs teams)
+"Description" (format sous-titre ?)
+Champ texte (chargé par le backend)
+</CardBody>
+ */}
+
+{/* <DIV>
+Title : "Mes Player Two"
+
+
+
+
+
+Une liste de Player 2 sous forme de Card à l'identique de la match page ((avec en plus un bouton vert si online, rouge si offline, si on a le temps d'intégrer les web sockets)
+
+
+</DIV> */}
+
+
+{/* <DIV>
+Champ texte : Liste de mes jeux
+
+Tableau sur 3 colonnes avec en header :
+
+plateforme
+jeux
+Image
+
+liste fournie par le backend au chargement de la page
+
+Possibilité de trier par ordre alphabétique sur les champs plateformes et jeux
+
+</DIV> */}
+
+
+{/* <DIV>
+Champ texte : "Mon fil d'actualité"
+Liste de flux d'actualité chargé par le backend
+</DIV> */}
+
+
+

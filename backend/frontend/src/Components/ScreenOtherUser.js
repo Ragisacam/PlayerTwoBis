895b2b8e432e65bc6944d9c3bf149425b4760c8f
Willy Zhou <wzhou471@gmail.com>
2020-03-02 15:18:01 +0100

    création react-router-dom

---
+++ backend/frontend/src/Components/ScreenOtherUser.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function ScreenOtherUser() {
+  return (
+          <h1>ScreenOtherUser</h1>
+    );
+}
+
+export default ScreenOtherUser;
+
+
+/*
+card user
+récuperer db avatar pseudo description
+fetch
+*/
+
+/*
+card ses jeux
+récupérer db liste des jeux du user
+fetch
+*/

47b7665f4ea35e0bab0a2738698a594428d26e41
Jerome Perret <jeromeperret@yahoo.fr>
2020-03-02 14:07:19 +0100

    match

---
+++ backend/frontend/Components/Inscription - Jeux.js
@@ -0,0 +1,19 @@
+// card
+
+// Plateforme
+// Champ de saisie
+
+// Service
+// Champ de saisie
+
+// Jeux
+// Champ de saisie
+
+// Tag
+// Champ de saisie
+
+// Progression bar
+
+// Bouton Save
+
+

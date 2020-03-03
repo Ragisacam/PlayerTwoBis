c4c110cd27991ad0decfa8f22d90b69b64284e28
Philippe Bizet <pbizet@gmail.com>
2020-03-03 15:10:13 +0100

    header v2

--- backend/frontend/src/Components/header.js
+++ backend/frontend/src/Components/header.js
@@ -3,25 +3,38 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import babyYoda from '../images/icons8-baby-yoda-48.png';
 import logo from '../images/logo-player2.png';
-import player2 from '../images/Player Two.png'
+import player2 from '../images/Player Two.png';
+import switchee from '../images/switchee.png'
 
 function header() {
 
   return(
 <nav className="headerFooter">
-<div class="d-flex flex-row">
-  <div>
-    <Link to="/ScreenHome">
-      <img src={logo} class="img-fluid" alt="logo"/>
-      </Link>
+<div class="container">
+  <div class="row align-items-start">
+      <div class="col ">
+        <Link to="/ScreenHome">
+          <img src={logo} class="img-fluid" alt="logo"/>
+          </Link>
+          <img class="img-fluid" src={player2} class="img-fluid" alt="logo"/>
+      </div>
+      
+      
+      <div class="col">
+      </div>
+
+      <div class="col align-items-end">
+          <img src={babyYoda} alt="babyYoda"/>
+          <img src={switchee} alt="switchee"/>
+          OUIIIIINNNNN
+      </div>
   </div>
-  <img class="img-fluid" src={player2} class="img-fluid" alt="logo"/>
 </div>
 
 
 {/* //avatar
   //redirection vers la page ScreenUser */}
-<img src={babyYoda} alt="babyYoda"/>
+
 
 {/* // button log
   // route sign in

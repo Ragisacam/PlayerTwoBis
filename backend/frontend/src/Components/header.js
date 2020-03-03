629e79ebfaffd0d15e17b6ae900ad4395cfdb904
Philippe Bizet <pbizet@gmail.com>
2020-03-03 12:37:45 +0100

    gangnam style

--- backend/frontend/src/Components/header.js
+++ backend/frontend/src/Components/header.js
@@ -3,16 +3,22 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import babyYoda from '../images/icons8-baby-yoda-48.png';
 import logo from '../images/logo-player2.png';
+import player2 from '../images/Player Two.png'
 
 function header() {
 
   return(
-<nav>
-  <Link to="/ScreenHome">
-<img src={logo} alt="logo"/>
-</Link>
+<nav className="headerFooter">
+<div class="d-flex flex-row">
+  <div>
+    <Link to="/ScreenHome">
+      <img src={logo} class="img-fluid" alt="logo"/>
+      </Link>
+  </div>
+  <img class="img-fluid" src={player2} class="img-fluid" alt="logo"/>
+</div>
 
-  
+
 {/* //avatar
   //redirection vers la page ScreenUser */}
 <img src={babyYoda} alt="babyYoda"/>

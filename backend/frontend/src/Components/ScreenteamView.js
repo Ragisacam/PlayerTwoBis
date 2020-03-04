a0c625f0cfebfa1bf303a1fca14d53fe14b45ea5
Zehekiel <guerin.quentin@live.fr>
2020-03-04 12:13:17 +0100

    screenteamviw static ok

--- backend/frontend/src/Components/ScreenteamView.js
+++ backend/frontend/src/Components/ScreenteamView.js
@@ -2,8 +2,9 @@ import React from 'react';
 import '../App.css';
 import {
   Card, CardImg, CardTitle, CardText, CardColumns,
-  CardSubtitle, CardBody, Row,
+  CardSubtitle, Row,
 } from 'reactstrap';
+import { Col } from 'react-bootstrap';
 
 
 function ScreenteamView() {
@@ -18,66 +19,106 @@ function ScreenteamView() {
       
 
       <CardColumns>
+                  
       {/*______ GAME ______*/}
       <Card style={{ backgroundColor: '#010212', borderRadius: "0px 50px" }}>
-        <CardBody style={{paddingTop: "10px", paddingBottom: "15px", paddingLeft:"15px"}}>
-          <CardTitle className="heading" >Game</CardTitle>
+        <Col  className="cardbody">
+          <CardTitle>Game</CardTitle>
           <CardText>Ici on joue à :</CardText>
-          <Row style={{paddingLeft: "15px", paddingTop: "15px", alignItems:"center"}}>
+          <Row style={{paddingLeft: "25px", paddingBottom: "15px", alignItems:"center"}}>
             <CardImg style={{height: '40px', width:'40px', borderRadius: "30px", marginRight: "10px"}} top width="100%" src={require('../images/bf5.jpg')} alt="game image 1" />
             <CardText>Battlefield 5 </CardText>
           </Row>
-          <Row style={{paddingLeft: "15px", paddingTop: "15px", alignItems:"center"}}>
+          <Row style={{paddingLeft: "25px", paddingBottom: "15px", alignItems:"center"}}>
             <CardImg style={{height: '40px', width:'40px', borderRadius: "30px", marginRight: "10px"}} top width="100%" src={require('../images/bf5.jpg')} alt="game image 2" />
             <CardText>Battlefield 5 </CardText>
           </Row>
           
-        </CardBody>
+        </Col>
       </Card>
 
+{/*______ ACTU ______ */}
+<Card style={{ backgroundColor: '#010212', borderRadius: "0px 50px", paddingBottom: "25px"}}>
+      <Col className="cardbody">
+          <CardTitle>Actu</CardTitle>
+          <CardSubtitle>news 1</CardSubtitle>
+          <CardText>- This is a wider card with supporting text below as a natural lead-in to additional content.</CardText>
+          <CardSubtitle>news 2</CardSubtitle>
+          <CardText>- This is a wider card with supporting text below as a natural lead-in to additional content. </CardText>
+          </Col>
+      </Card>
+
+
+      {/* Philosophie */}
       <Card style={{ backgroundColor: '#010212', borderRadius: "0px 50px" }}>
-        <CardBody >
+        <Col className="cardbody">
           <CardTitle>Philosophie</CardTitle>
           <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-        </CardBody>
+        </Col>
       </Card>
 
       {/*______ Description ______ */}
       <Card style={{ backgroundColor: '#010212', borderRadius: "0px 50px" }}>
-        <CardTitle className="heading" >Description</CardTitle>
-        <CardText style={{marginBottom: "30px"}}>With supporting text below as a natural lead-in to additional content.</CardText>
+      <Col className="cardbody">
+        <CardTitle >Description</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+      </Col>
       </Card>
 
-      {/*______ ACTU ______ */}
-      <Card style={{ backgroundColor: '#010212', borderRadius: "0px 50px", paddingBottom: "25px"}}>
-        
-          <CardTitle>Actu</CardTitle>
-          <CardSubtitle>news 1</CardSubtitle>
-          <CardText>- This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <CardSubtitle>news 2</CardSubtitle>
-          <CardText>- This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-        
-      </Card>
+
 
       {/* ______ MEMBRE ______*/}
       <Card style={{ backgroundColor: '#010212', borderRadius: "0px 50px" }}>
-        <CardTitle>Membre</CardTitle>
-        <Row style={{flexDirection:Row}}>
-          <CardSubtitle>Admin</CardSubtitle>
-          <CardText> 1</CardText>
-        </Row>
+        <Col className="cardbody">
+          <CardTitle>Membre</CardTitle>
+
+        {/* ADMIN */}
+        <Row className="usertitle">
+          <CardSubtitle style={{margin:"0px"}}>Admin</CardSubtitle> 
+          <CardText>nb: 1</CardText>
+        </Row> 
         
-          {/* <div>
-          <CardImg style={{height: '20px', width:'20px', marginRight: "10px"}} top width="100%" src={require('../images/user-solid.svg')} alt="Card image cap" />
+          <Row className="user">
+            <CardImg style={{height: '20px', width:'20px', marginRight: "10px"}} top width="100%" src={require('../images/user-solid.svg')} alt="Card image cap" />
+            <CardText>Username 1</CardText>
+          </Row>
 
-          </div> */}
+        {/* SOUS ADMIN */}
+        <Row className="usertitle">
+          <CardSubtitle style={{margin:"0px"}}>Sous Admin</CardSubtitle> 
+          <CardText>nb: 2</CardText>
+        </Row> 
 
-        <CardSubtitle>Sous Admin</CardSubtitle>
+          <Row className="user">
+            <CardImg style={{height: '20px', width:'20px', marginRight: "10px"}} top width="100%" src={require('../images/user-solid.svg')} alt="Card image cap" />
+            <CardText>Sous Admin 1</CardText>
+          </Row>
+          <Row className="user">
+            <CardImg style={{height: '20px', width:'20px', marginRight: "10px"}} top width="100%" src={require('../images/user-solid.svg')} alt="Card image cap" />
+            <CardText>Sous Admin 2</CardText>
+          </Row>
 
-        <CardSubtitle>Regular</CardSubtitle>
+        {/* Member */}
+        <Row className="usertitle">
+          <CardSubtitle style={{margin:"0px"}}>Sous Admin</CardSubtitle> 
+          <CardText>nb: 3</CardText>
+        </Row>
 
-        <CardText>With supporting text below as a natural lead-in to additional content.>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Row className="user">
+            <CardImg style={{height: '20px', width:'20px', marginRight: "10px"}} top width="100%" src={require('../images/user-solid.svg')} alt="Card image cap" />
+            <CardText>Member 1</CardText>
+          </Row>
 
+          <Row className="user">
+            <CardImg style={{height: '20px', width:'20px', marginRight: "10px"}} top width="100%" src={require('../images/user-solid.svg')} alt="Card image cap" />
+            <CardText>Member 2</CardText>
+          </Row>
+          <Row className="user">
+            <CardImg style={{height: '20px', width:'20px', marginRight: "10px"}} top width="100%" src={require('../images/user-solid.svg')} alt="Card image cap" />
+            <CardText>Member 3</CardText>
+          </Row>
+        </Col>
+        
       </Card>
 
     </CardColumns>
